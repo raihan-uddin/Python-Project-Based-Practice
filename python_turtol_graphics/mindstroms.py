e9026d45b4896cde75f2341df3280dbf328c4f02
Raihan Uddin <blackedition.me@gmail.com>
2017-04-22 00:51:39 +0600

    Draw a circle from square

--- python_turtol_graphics/mindstroms.py
+++ python_turtol_graphics/mindstroms.py
@@ -5,23 +5,23 @@ def drw_square(some_turtle):
         some_turtle.forward(100)
         some_turtle.right(90)
 
-
 def draw_art():
     window = turtle.Screen()
     window.bgcolor("red")
 
-    #Create the turtle Brad - Draws a square
+    # Create the turtle Brad - Draws a square
     brad = turtle.Turtle()
     brad.shape("turtle")
     brad.color("yellow")
-    brad.speed(2)
-    drw_square(brad)
-
-    #Create the turtle Angie - Draws a circle
-    angie = turtle.Turtle()
-    angie.shape("arrow")
-    angie.color("white")
-    angie.circle(100)
+    brad.speed(4)
+    for i in range(1,37):
+        drw_square(brad)
+        brad.right(10)
+    # Create the turtle Angie - Draws a circle
+    # angie = turtle.Turtle()
+    # angie.shape("arrow")
+    # angie.color("white")
+    # angie.circle(100)
     window.exitonclick()
 
 draw_art()
