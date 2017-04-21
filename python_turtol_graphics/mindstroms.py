4f012fa8644a9d9a19730a26b18891bf7910ca73
Raihan Uddin <blackedition.me@gmail.com>
2017-04-21 23:46:50 +0600

    Draw a Square with Turtle

---
+++ python_turtol_graphics/mindstroms.py
@@ -0,0 +1,22 @@
+import turtle
+
+def drw_square():
+    window = turtle.Screen()
+    window.bgcolor("red")
+
+    brad = turtle.Turtle()
+    brad.shape("turtle")
+    brad.color("yellow")
+    brad.speed(2)
+
+    brad.forward(100)
+    brad.right(90)
+    brad.forward(100)
+    brad.right(90)
+    brad.forward(100)
+    brad.right(90)
+    brad.forward(100)
+
+    window.exitonclick()
+
+drw_square()
