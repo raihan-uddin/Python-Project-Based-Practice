dca991be6fa1ec701cb901e820c27d924fffa4d8
Raihan Uddin <blackedition.me@gmail.com>
2017-04-21 23:57:13 +0600

    Improving Code Quality

--- python_turtol_graphics/mindstroms.py
+++ python_turtol_graphics/mindstroms.py
@@ -1,22 +1,27 @@
 import turtle
 
-def drw_square():
+def drw_square(some_turtle):
+    for i in range(1,5):
+        some_turtle.forward(100)
+        some_turtle.right(90)
+
+
+def draw_art():
     window = turtle.Screen()
     window.bgcolor("red")
 
+    #Create the turtle Brad - Draws a square
     brad = turtle.Turtle()
     brad.shape("turtle")
     brad.color("yellow")
     brad.speed(2)
+    drw_square(brad)
 
-    brad.forward(100)
-    brad.right(90)
-    brad.forward(100)
-    brad.right(90)
-    brad.forward(100)
-    brad.right(90)
-    brad.forward(100)
-
+    #Create the turtle Angie - Draws a circle
+    angie = turtle.Turtle()
+    angie.shape("arrow")
+    angie.color("white")
+    angie.circle(100)
     window.exitonclick()
 
-drw_square()
+draw_art()
