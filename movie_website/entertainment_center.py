9cb875981aa88179cb8a1eea504102526e55889d
Raihan Uddin <blackedition.me@gmail.com>
2017-04-22 14:59:31 +0600

    added a template named fresh_tomatoes.html

--- movie_website/entertainment_center.py
+++ movie_website/entertainment_center.py
@@ -1,16 +1,40 @@
 from movie_website import media
+from movie_website import fresh_tomatoes
 
 toy_story = media.Movie("Toy Story",
                         "A story of a boy and his toys that come to life",
                         "https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
                         "https://www.youtube.com/watch?v=JcpWXaA2qeg")
 
-print(toy_story.storyline)
+#print(toy_story.storyline)
 
 avatar = media.Movie("Avatar",
                         "A marine on an aline planet",
                         "https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg",
                         "https://www.youtube.com/watch?v=EzETGqZN6dU")
-print(avatar.storyline)
+#print(avatar.storyline)
+#avatar.show_trailer()
 
-avatar.show_trailer()
+school_of_rock = media.Movie("School of Rock",
+                             "Using rock music to learn",
+                             "http://upload.wikimedia.org/wikipedia/en/1/11/School_of_Rock_Poster.jpg",
+                             "https://www.youtube.com/watch?v=3PsUJFEBC74")
+
+ratatouille = media.Movie("Rataouille",
+                             "Storyline",
+                             "http://upload.wikimedia.org/wikipedia/en/1/11/School_of_Rock_Poster.jpg",
+                             "https://www.youtube.com/watch?v=3PsUJFEBC74")
+
+midnight_in_paris = media.Movie("Midnight in paris",
+                             "Using rock music to learn",
+                             "http://upload.wikimedia.org/wikipedia/en/1/11/School_of_Rock_Poster.jpg",
+                             "https://www.youtube.com/watch?v=3PsUJFEBC74")
+
+hunger_games = media.Movie("Hunger games"
+                           "",
+                             "Using rock music to learn",
+                             "http://upload.wikimedia.org/wikipedia/en/1/11/School_of_Rock_Poster.jpg",
+                             "https://www.youtube.com/watch?v=3PsUJFEBC74")
+
+movies = [toy_story, avatar, school_of_rock, ratatouille, midnight_in_paris, hunger_games]
+fresh_tomatoes.open_movies_page(movies)
