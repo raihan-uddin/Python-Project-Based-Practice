acf3c992fd52444b2a579a875da6822a0f4c2906
Raihan Uddin <blackedition.me@gmail.com>
2017-04-22 12:01:44 +0600

    checking embarrassing story

---
+++ embarrassing_story/check_profanity.py
@@ -0,0 +1,25 @@
+import urllib
+from urllib import request
+from urllib.parse import quote
+
+
+def read_text():
+    filepath = r"F:\PycharmProjects\UdacityPythonPractice\embarrassing_story\movie_quotes.txt";
+    quotes = open(filepath, newline='', encoding='utf-8')
+    contents_of_file = quotes.read()
+    # print(contents_of_file)
+    check_profanity(contents_of_file)
+
+def check_profanity(text_to_check):
+    url = "http://www.wdylike.appspot.com/?q=" + quote(text_to_check)
+    with urllib.request.urlopen(url) as connection:
+        print(connection)
+        output = connection.read().decode('utf-8')
+    print(output)
+    connection.close()
+
+# with urllib.request.urlopen('http://www.python.org/') as f:
+#     print(f.read(900).decode('utf-8'))
+
+
+read_text()
