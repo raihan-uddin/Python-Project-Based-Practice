d79c98a6bd40da4ccc540fb0eb1aa91f0e644884
Raihan Uddin <blackedition.me@gmail.com>
2017-04-23 12:50:56 +0600

    Inheritance in python

---
+++ inheritance_in_python/inheritance.py
@@ -0,0 +1,25 @@
+class Parent:
+    def __init__(self, last_name, eye_color):
+        print("Parent Constructor Called")
+        self.last_name = last_name
+        self.eye_color = eye_color
+
+    def show_info(self):
+        print("Last Name - " + self.last_name)
+        print("Eye color - " + self.eye_color)
+
+
+class Child(Parent):
+    def __init__(self, last_name, eye_color, num_of_toys):
+        print("Child Constructor Called")
+        Parent.__init__(self, last_name, eye_color)
+        self.number_of_toys = num_of_toys
+
+
+billy_cyrus = Parent("Cyrus", "blue")
+# print(billy_cyrus.last_name)
+billy_cyrus.show_info()
+
+# miley_cyrus = Child("Cyrus", "Blue", 5)
+# print(miley_cyrus.last_name)
+# print(miley_cyrus.number_of_toys)
